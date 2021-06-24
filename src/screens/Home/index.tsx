29ca14a6ab442df59980d74aa82b68802480818a
unknown <alngregory@gmail.com>
2021-06-23 21:18:02 -0300

    Aula 03 - Inputs, detalhes de guildas e modal

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,83 +1,93 @@
 import React, { useState } from 'react';
 import { View, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
-import { Profile } from '../../components/Profile';
+import { Appointment } from '../../components/Appointment';
 import { ListDivider } from '../../components/ListDivider';
 import { ListHeader } from '../../components/ListHeader';
-import { Appointment } from '../../components/Appointment';
+import { Background } from '../../components/Background';
+import { ButtonAdd } from '../../components/ButtonAdd';
+import { Profile } from '../../components/Profile';
 
 import { styles } from './styles';
 
 export function Home() {
-    const [category, setCategory] = useState('');
+  const [category, setCategory] = useState('');
 
-    const appointments = [
-        {
-            id: '1',
-            guild: {
-                id: '1',
-                name: 'Lendários',
-                icon: null,
-                owner: true
-            },
-            category: '1',
-            date: '22/06 às 20h40',
-            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
-        },
-        {
-            id: '2',
-            guild: {
-              id: '1',
-              name: 'Lendários',
-              icon: null,
-              owner: true
-            },
-            category: '1',
-            date: '22/06 às 20:40h',
-            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'      
-          }
-    ]
+  const navigation = useNavigation();
 
-    function handleCategorySelect(categoryId: string) {
-        categoryId === category ? setCategory('') : setCategory;
-    }
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'      
+    },
+    {
+      id: '2',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'      
+    },
+  ]
 
-    return (
-        <View>
-            <View style={styles.header}>
-                <Profile />
-                <ButtonAdd />
-            </View>
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  } 
+  
+  function handleAppointmentDetails() {
+    navigation.navigate('AppointmentDetails');
+  } 
+  
+  function handleAppointmentCreate() {
+    navigation.navigate('AppointmentCreate');
+  }  
 
-            <View>
-                <CategorySelect
-                    categorySelected={category}
-                    setCategory={handleCategorySelect}
-                />
+  return (
+    <Background>
+      <View style={styles.header}>
+        <Profile />
+        <ButtonAdd onPress={handleAppointmentCreate}/>
+      </View>
+    
+      <CategorySelect 
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
 
-                <View
-                    style={styles.content}
-                >
-                    <ListHeader
-                        title="Partidas agendadas"
-                        subtitle="Total 6"
-                    />
+      <View style={styles.content}>
+        <ListHeader 
+          title="Partidas agendadas"
+          subtitle="Total 6"
+        />
 
-                    <FlatList
-                        data={appointments}
-                        keyExtractor={item => item.id}
-                        renderItem={({ item }) => (
-                            <Appointment data={item} />
-                        )}
-                        ItemSeparatorComponent={() => <ListDivider />}
-                        style={styles.matches}
-                        showsVerticalScrollIndicator={false}
-                    />
-                </View>
-
-            </View>
-        </View>
-    );
+        <FlatList 
+            data={appointments}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+            <Appointment 
+              data={item} 
+              onPress={handleAppointmentDetails}
+            />            
+          )}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </Background>
+  );  
 }
