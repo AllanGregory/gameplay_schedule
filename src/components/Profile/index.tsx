f46d6eb62d4acfa32504456558c4b17ce68bcb07
unknown <alngregory@gmail.com>
2021-06-24 20:57:18 -0300

    Aula 04 - Autenticação com oAuth2, rodas públicas e privadas, contexto e exibição de dados

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,30 +1,37 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import { useAuth } from '../../hooks/auth';
+
 import { Avatar } from '../Avatar';
 import { styles } from './styles';
 
 export function Profile() {
-    return (
-        <View style={styles.container}>
+  const { user } = useAuth();
+  console.log(user)
 
-            <Avatar urlImage="https://github.com/AllanGregory.png"/>
+  return (
+    <View style={styles.container}>
 
-            <View>
-                <View style={styles.user}>
-                    <Text style={styles.greeting}>
-                        Olá
-                    </Text>
+      <Avatar urlImage={user.avatar} />
 
-                    <Text style={styles.username}>
-                        Kabelo
-                    </Text>
-                </View>
-
-                <Text style={styles.message}>
-                    Hoje é dia de vitória!
-                </Text>
-            </View>
+      <View>
+        <View style={styles.user}>
+          <Text style={styles.greeting}>
+            Olá,
+          </Text>
+          
+          <Text style={styles.username}>
+            { user.firstName }
+          </Text>
         </View>
-    )
+
+        <Text style={styles.message}>
+          Hoje é dia de vitória
+        </Text>
+      </View>
+
+    </View>
+  )
+
 }
